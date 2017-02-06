b8b4d261a43f37ba8d6f8ea6a7653bf7aa59ac26
bhovhannes <bhovhannes@gmail.com>
2017-02-06 15:21:53 +0400

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# workfront-api-objcodes
-Object codes to be used with Workfront API
+# workfront-objcodes
+
+[![NPM version][npm-version-image]][npm-url] [![NPM downloads][npm-downloads-image]][npm-url] [![Apache v2 License][license-image]][license-url] [![Build Status][travis-image]][travis-url]
+
+Definitions for object codes to be used with Workfront API
+
+## Usage
+
+Package export 2 types of bundles:
+
+ `dist/objcodes.js` - is ES6 module (ES6 import/export and ES5 code), which exports separate definitions for all object codes and also a single object named `ObjCodes` which contains values of all object codes.  
+ This bundle is the default package entry point.
+ 
+`dist/umb/objcodes.js` - is UMD bundle for ES5 environments.
+
+We recommend to use `dist/objcodes.js` as it will allow tree-shaking feature of your favorite bundler to eliminate unused constants from the target bundle.  
+The code inside `dist/objcodes.js` is in ES5, so you don't need transpilers to use it.
+
+
+### Examples
+
+```javascript
+import {Baseline} from 'workfront-objcodes'
+
+// output 'BLIN'
+console.log(Baseline)
+```
+
+```javascript
+import {ObjCodes} from 'workfront-objcodes'
+
+// outputs 'BLIN'
+console.log(ObjCodes.Baseline)
+```
+
+```typescript
+import {TObjCode} from 'workfront-objcodes'
+
+// TS2322: Type '"FOO"' is not assignable to type 'TObjCode' 
+const myObjCode: TObjCode = 'FOO'
+```
+
+
+## License
+
+Copyright (c) 2017 Workfront
+
+Licensed under the Apache License, Version 2.0.
+See the top-level file `LICENSE` and
+(http://www.apache.org/licenses/LICENSE-2.0).
+
+
+[license-image]: http://img.shields.io/badge/license-APv2-blue.svg?style=flat
+[license-url]: LICENSE
+
+[npm-url]: https://www.npmjs.org/package/workfront-objcodes
+[npm-version-image]: https://img.shields.io/npm/v/workfront-objcodes.svg?style=flat
+[npm-downloads-image]: https://img.shields.io/npm/dm/workfront-objcodes.svg?style=flat
+
+[travis-url]: https://travis-ci.org/Workfront/workfront-objcodes
+[travis-image]: https://img.shields.io/travis/Workfront/workfront-objcodes.svg?style=flat
---
+++ src/objcodes.ts
@@ -0,0 +1,442 @@
+/**
+ * Copyright 2017 Workfront
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {TObjCode} from '../typings/TObjCode'
+import {TObjCodes} from '../typings/TObjCodes'
+
+export const AccessLevel: TObjCode = 'ACSLVL'
+export const AccessLevelPermissions: TObjCode = 'ALVPER'
+export const AccessRequest: TObjCode = 'ACSREQ'
+export const AccessRule: TObjCode = 'ACSRUL'
+export const AccessRulePreference: TObjCode = 'ARPREF'
+export const AccessScope: TObjCode = 'ACSCP'
+export const AccessScopeAction: TObjCode = 'ASCPAT'
+export const AccessToken: TObjCode = 'ACSTOK'
+export const AccountRep: TObjCode = 'ACNTRP'
+export const Acknowledgement: TObjCode = 'ACK'
+export const AgileView: TObjCode = 'AGILEV'
+export const Announcement: TObjCode = 'ANCMNT'
+export const AnnouncementAttachment: TObjCode = 'ANMATT'
+export const AnnouncementOptOut: TObjCode = 'AMNTO'
+export const AnnouncementRecipient: TObjCode = 'ANCREC'
+export const AppBuild: TObjCode = 'APPBLD'
+export const AppEvent: TObjCode = 'APEVT'
+export const AppGlobal: TObjCode = 'APGLOB'
+export const AppInfo: TObjCode = 'APPINF'
+export const Approval: TObjCode = 'APPROVAL'
+export const ApprovalPath: TObjCode = 'ARVPTH'
+export const ApprovalProcess: TObjCode = 'ARVPRC'
+export const ApprovalProcessAttachable: TObjCode = 'APRPROCATCH'
+export const ApprovalStep: TObjCode = 'ARVSTP'
+export const ApproverStatus: TObjCode = 'ARVSTS'
+export const Assignment: TObjCode = 'ASSGN'
+export const AuditLoginAsSession: TObjCode = 'AUDS'
+export const Authentication: TObjCode = 'AUTH'
+export const Avatar: TObjCode = 'AVATAR'
+export const AwaitingApproval: TObjCode = 'AWAPVL'
+export const BackgroundJob: TObjCode = 'BKGJOB'
+export const Baseline: TObjCode = 'BLIN'
+export const BaselineTask: TObjCode = 'BSTSK'
+export const BillingRecord: TObjCode = 'BILL'
+export const Branding: TObjCode = 'BRND'
+export const BurndownEvent: TObjCode = 'BDNEVT'
+export const CalendarEvent: TObjCode = 'CALEVT'
+export const CalendarFeedEntry: TObjCode = 'CALITM'
+export const CalendarInfo: TObjCode = 'CALEND'
+export const CalendarPortalSection: TObjCode = 'CALPTL'
+export const CalendarSection: TObjCode = 'CALSEC'
+export const CallableExpression: TObjCode = 'CALEXP'
+export const CardField: TObjCode = 'LTMPLC'
+export const Category: TObjCode = 'CTGY'
+export const CategoryAccessRule: TObjCode = 'CATACR'
+export const CategoryCascadeRule: TObjCode = 'CTCSRL'
+export const CategoryCascadeRuleMatch: TObjCode = 'CTCSRM'
+export const CategoryParameter: TObjCode = 'CTGYPA'
+export const CategoryParameterExpression: TObjCode = 'CTGPEX'
+export const Company: TObjCode = 'CMPY'
+export const ComponentKey: TObjCode = 'CMPSRV'
+export const ContextSensitiveHelp: TObjCode = 'CXTHP'
+export const CustomEnum: TObjCode = 'CSTEM'
+export const CustomEnumOrder: TObjCode = 'CSTEMO'
+export const CustomLabel: TObjCode = 'LTMCL'
+export const CustomMenu: TObjCode = 'CSTMNU'
+export const CustomMenuCustomMenu: TObjCode = 'CMSCMS'
+export const CustomQuarter: TObjCode = 'CSTQRT'
+export const Customer: TObjCode = 'CUST'
+export const CustomerDeletion: TObjCode = 'CUSDEL'
+export const CustomerFeedback: TObjCode = 'CSFD'
+export const CustomerPreferences: TObjCode = 'CUSTPR'
+export const CustomerTimelineCalc: TObjCode = 'CPTC'
+export const CustsSections: TObjCode = 'CSTSEC'
+export const DocMetadataLink: TObjCode = 'DMETAL'
+export const DocMetadataLinkGroup: TObjCode = 'DMETAG'
+export const DocsFolders: TObjCode = 'DOCFLD'
+export const Document: TObjCode = 'DOCU'
+export const DocumentApproval: TObjCode = 'DOCAPL'
+export const DocumentFolder: TObjCode = 'DOCFDR'
+export const DocumentProvider: TObjCode = 'DOCPRO'
+export const DocumentProviderConfig: TObjCode = 'DOCCFG'
+export const DocumentProviderMetadata: TObjCode = 'DOCMET'
+export const DocumentRequest: TObjCode = 'DOCREQ'
+export const DocumentShare: TObjCode = 'DOCSHR'
+export const DocumentTaskStatus: TObjCode = 'DOCTSK'
+export const DocumentVersion: TObjCode = 'DOCV'
+export const Email: TObjCode = 'EMAILC'
+export const EmailTemplate: TObjCode = 'EMLTPL'
+export const Endorsement: TObjCode = 'ENDR'
+export const EndorsementShare: TObjCode = 'ENDSHR'
+export const EventHandler: TObjCode = 'EVNTH'
+export const EventSubscription: TObjCode = 'EVTSUB'
+export const EwsFileHandle: TObjCode = 'EWS'
+export const ExchangeRate: TObjCode = 'EXRATE'
+export const Expense: TObjCode = 'EXPNS'
+export const ExpenseType: TObjCode = 'EXPTYP'
+export const ExternalDocument: TObjCode = 'EXTDOC'
+export const ExternalSection: TObjCode = 'EXTSEC'
+export const Favorite: TObjCode = 'FVRITE'
+export const Feature: TObjCode = 'FEATR'
+export const FinancialData: TObjCode = 'FINDAT'
+export const Group: TObjCode = 'GROUP'
+export const Hour: TObjCode = 'HOUR'
+export const HourType: TObjCode = 'HOURT'
+export const IPRange: TObjCode = 'IPRAGE'
+export const ImportRow: TObjCode = 'IROW'
+export const ImportTemplate: TObjCode = 'ITMPL'
+export const InstalledDDItem: TObjCode = 'IDDI'
+export const Iteration: TObjCode = 'ITRN'
+export const JournalEntry: TObjCode = 'JRNLE'
+export const JournalField: TObjCode = 'JRNLF'
+export const KickStart: TObjCode = 'KSS'
+export const LayoutTemplate: TObjCode = 'LYTMPL'
+export const LayoutTemplateDatePreference: TObjCode = 'LTMPDP'
+export const LayoutTemplatePage: TObjCode = 'LTMPLP'
+export const LicenseOrder: TObjCode = 'LICEOR'
+export const Like: TObjCode = 'LIKE'
+export const LinkedFolder: TObjCode = 'LNKFDR'
+export const MasterTask: TObjCode = 'MTSK'
+export const MessageArg: TObjCode = 'MSGARG'
+export const MetaRecord: TObjCode = 'PRSTOBJ'
+export const Milestone: TObjCode = 'MILE'
+export const MilestonePath: TObjCode = 'MPATH'
+export const MobileDevice: TObjCode = 'MOBILDVC'
+export const NonWorkDay: TObjCode = 'NONWKD'
+export const Note: TObjCode = 'NOTE'
+export const NoteTag: TObjCode = 'NTAG'
+export const NotificationPreference: TObjCode = 'NOTPRF'
+export const NotificationRecord: TObjCode = 'TMNR'
+export const ObjectCategory: TObjCode = 'OBJCAT'
+export const OpTask: TObjCode = 'OPTASK'
+export const Parameter: TObjCode = 'PARAM'
+export const ParameterDescriptiveText: TObjCode = 'PRMDTX'
+export const ParameterGroup: TObjCode = 'PGRP'
+export const ParameterOption: TObjCode = 'POPT'
+export const ParameterValue: TObjCode = 'PVAL'
+export const PopAccount: TObjCode = 'POPA'
+export const PortalProfile: TObjCode = 'PTLPFL'
+export const PortalSection: TObjCode = 'PTLSEC'
+export const PortalTab: TObjCode = 'PTLTAB'
+export const PortalTabSection: TObjCode = 'PRTBSC'
+export const Portfolio: TObjCode = 'PORT'
+export const Predecessor: TObjCode = 'PRED'
+export const Preference: TObjCode = 'PROSET'
+export const PrivateCustomerPref: TObjCode = 'PCPREF'
+export const Program: TObjCode = 'PRGM'
+export const Project: TObjCode = 'PROJ'
+export const ProjectUser: TObjCode = 'PRTU'
+export const ProjectUserRole: TObjCode = 'PTEAM'
+export const Proof: TObjCode = 'PROOF'
+export const QueueDef: TObjCode = 'QUED'
+export const QueueTopic: TObjCode = 'QUET'
+export const QueueTopicGroup: TObjCode = 'QUETGP'
+export const Rate: TObjCode = 'RATE'
+export const Recent: TObjCode = 'RECENT'
+export const RecentMenuItem: TObjCode = 'RECENTMENUITEM'
+export const RecentUpdate: TObjCode = 'RUPDTE'
+export const RecurrenceRule: TObjCode = 'RECR'
+export const RemovedDocumentVersion: TObjCode = 'RMVDOV'
+export const ReportFolder: TObjCode = 'RPTFDR'
+export const Reseller: TObjCode = 'RSELR'
+export const ReservedTime: TObjCode = 'RESVT'
+export const ResourceAllocation: TObjCode = 'RSALLO'
+export const ResourceContour: TObjCode = 'RSCONT'
+export const ResourceManager: TObjCode = 'RESMGR'
+export const ResourcePool: TObjCode = 'RSPOOL'
+export const ResourceSchedulingFilter: TObjCode = 'RSFT'
+export const RestorableObject: TObjCode = 'RSTOBJ'
+export const Risk: TObjCode = 'RISK'
+export const RiskType: TObjCode = 'RSKTYP'
+export const Role: TObjCode = 'ROLE'
+export const RoutingRule: TObjCode = 'RRUL'
+export const S3Migration: TObjCode = 'S3MT'
+export const SSOMapping: TObjCode = 'SSOMAP'
+export const SSOMappingRule: TObjCode = 'SSOMR'
+export const SSOOption: TObjCode = 'SSOPT'
+export const SSOUsername: TObjCode = 'SSOUSR'
+export const SandboxMigration: TObjCode = 'SNDMG'
+export const Schedule: TObjCode = 'SCHED'
+export const ScheduledReport: TObjCode = 'SCHREP'
+export const ScoreCard: TObjCode = 'SCORE'
+export const ScoreCardAnswer: TObjCode = 'SCANS'
+export const ScoreCardOption: TObjCode = 'SCOPT'
+export const ScoreCardQuestion: TObjCode = 'SCOREQ'
+export const SearchEvent: TObjCode = 'SRCEVT'
+export const SecurityAncestor: TObjCode = 'SECANC'
+export const Sequence: TObjCode = 'SEQ'
+export const SharingSettings: TObjCode = 'SHRSET'
+export const StepApprover: TObjCode = 'SPAPVR'
+export const Task: TObjCode = 'TASK'
+export const Team: TObjCode = 'TEAMOB'
+export const TeamMember: TObjCode = 'TEAMMB'
+export const TeamMemberRole: TObjCode = 'TEAMMR'
+export const Template: TObjCode = 'TMPL'
+export const TemplateAssignment: TObjCode = 'TASSGN'
+export const TemplatePredecessor: TObjCode = 'TPRED'
+export const TemplateTask: TObjCode = 'TTSK'
+export const TemplateUser: TObjCode = 'TMTU'
+export const TemplateUserRole: TObjCode = 'TTEAM'
+export const TimedNotification: TObjCode = 'TMNOT'
+export const Timesheet: TObjCode = 'TSHET'
+export const TimesheetProfile: TObjCode = 'TSPRO'
+export const TimesheetTemplate: TObjCode = 'TSHTMP'
+export const UIFilter: TObjCode = 'UIFT'
+export const UIGroupBy: TObjCode = 'UIGB'
+export const UIView: TObjCode = 'UIVW'
+export const Update: TObjCode = 'UPDATE'
+export const User: TObjCode = 'USER'
+export const UserActivity: TObjCode = 'USERAC'
+export const UserAvailability: TObjCode = 'USRAVL'
+export const UserDelegation: TObjCode = 'USRDEL'
+export const UserGroups: TObjCode = 'USRGPS'
+export const UserNote: TObjCode = 'USRNOT'
+export const UserObjectPref: TObjCode = 'USOP'
+export const UserPrefValue: TObjCode = 'USERPF'
+export const UserResource: TObjCode = 'USERRS'
+export const UsersSections: TObjCode = 'USRSEC'
+export const Work: TObjCode = 'WORK'
+export const WorkItem: TObjCode = 'WRKITM'
+
+export const ObjCodes: TObjCodes = {
+    AccessLevel: AccessLevel,
+    AccessLevelPermissions: AccessLevelPermissions,
+    AccessRequest: AccessRequest,
+    AccessRule: AccessRule,
+    AccessRulePreference: AccessRulePreference,
+    AccessScope: AccessScope,
+    AccessScopeAction: AccessScopeAction,
+    AccessToken: AccessToken,
+    AccountRep: AccountRep,
+    Acknowledgement: Acknowledgement,
+    AgileView: AgileView,
+    Announcement: Announcement,
+    AnnouncementAttachment: AnnouncementAttachment,
+    AnnouncementOptOut: AnnouncementOptOut,
+    AnnouncementRecipient: AnnouncementRecipient,
+    AppBuild: AppBuild,
+    AppEvent: AppEvent,
+    AppGlobal: AppGlobal,
+    AppInfo: AppInfo,
+    Approval: Approval,
+    ApprovalPath: ApprovalPath,
+    ApprovalProcess: ApprovalProcess,
+    ApprovalProcessAttachable: ApprovalProcessAttachable,
+    ApprovalStep: ApprovalStep,
+    ApproverStatus: ApproverStatus,
+    Assignment: Assignment,
+    AuditLoginAsSession: AuditLoginAsSession,
+    Authentication: Authentication,
+    Avatar: Avatar,
+    AwaitingApproval: AwaitingApproval,
+    BackgroundJob: BackgroundJob,
+    Baseline: Baseline,
+    BaselineTask: BaselineTask,
+    BillingRecord: BillingRecord,
+    Branding: Branding,
+    BurndownEvent: BurndownEvent,
+    CalendarEvent: CalendarEvent,
+    CalendarFeedEntry: CalendarFeedEntry,
+    CalendarInfo: CalendarInfo,
+    CalendarPortalSection: CalendarPortalSection,
+    CalendarSection: CalendarSection,
+    CallableExpression: CallableExpression,
+    CardField: CardField,
+    Category: Category,
+    CategoryAccessRule: CategoryAccessRule,
+    CategoryCascadeRule: CategoryCascadeRule,
+    CategoryCascadeRuleMatch: CategoryCascadeRuleMatch,
+    CategoryParameter: CategoryParameter,
+    CategoryParameterExpression: CategoryParameterExpression,
+    Company: Company,
+    ComponentKey: ComponentKey,
+    ContextSensitiveHelp: ContextSensitiveHelp,
+    CustomEnum: CustomEnum,
+    CustomEnumOrder: CustomEnumOrder,
+    CustomLabel: CustomLabel,
+    CustomMenu: CustomMenu,
+    CustomMenuCustomMenu: CustomMenuCustomMenu,
+    CustomQuarter: CustomQuarter,
+    Customer: Customer,
+    CustomerDeletion: CustomerDeletion,
+    CustomerFeedback: CustomerFeedback,
+    CustomerPreferences: CustomerPreferences,
+    CustomerTimelineCalc: CustomerTimelineCalc,
+    CustsSections: CustsSections,
+    DocMetadataLink: DocMetadataLink,
+    DocMetadataLinkGroup: DocMetadataLinkGroup,
+    DocsFolders: DocsFolders,
+    Document: Document,
+    DocumentApproval: DocumentApproval,
+    DocumentFolder: DocumentFolder,
+    DocumentProvider: DocumentProvider,
+    DocumentProviderConfig: DocumentProviderConfig,
+    DocumentProviderMetadata: DocumentProviderMetadata,
+    DocumentRequest: DocumentRequest,
+    DocumentShare: DocumentShare,
+    DocumentTaskStatus: DocumentTaskStatus,
+    DocumentVersion: DocumentVersion,
+    Email: Email,
+    EmailTemplate: EmailTemplate,
+    Endorsement: Endorsement,
+    EndorsementShare: EndorsementShare,
+    EventHandler: EventHandler,
+    EventSubscription: EventSubscription,
+    EwsFileHandle: EwsFileHandle,
+    ExchangeRate: ExchangeRate,
+    Expense: Expense,
+    ExpenseType: ExpenseType,
+    ExternalDocument: ExternalDocument,
+    ExternalSection: ExternalSection,
+    Favorite: Favorite,
+    Feature: Feature,
+    FinancialData: FinancialData,
+    Group: Group,
+    Hour: Hour,
+    HourType: HourType,
+    IPRange: IPRange,
+    ImportRow: ImportRow,
+    ImportTemplate: ImportTemplate,
+    InstalledDDItem: InstalledDDItem,
+    Iteration: Iteration,
+    JournalEntry: JournalEntry,
+    JournalField: JournalField,
+    KickStart: KickStart,
+    LayoutTemplate: LayoutTemplate,
+    LayoutTemplateDatePreference: LayoutTemplateDatePreference,
+    LayoutTemplatePage: LayoutTemplatePage,
+    LicenseOrder: LicenseOrder,
+    Like: Like,
+    LinkedFolder: LinkedFolder,
+    MasterTask: MasterTask,
+    MessageArg: MessageArg,
+    MetaRecord: MetaRecord,
+    Milestone: Milestone,
+    MilestonePath: MilestonePath,
+    MobileDevice: MobileDevice,
+    NonWorkDay: NonWorkDay,
+    Note: Note,
+    NoteTag: NoteTag,
+    NotificationPreference: NotificationPreference,
+    NotificationRecord: NotificationRecord,
+    ObjectCategory: ObjectCategory,
+    OpTask: OpTask,
+    Parameter: Parameter,
+    ParameterDescriptiveText: ParameterDescriptiveText,
+    ParameterGroup: ParameterGroup,
+    ParameterOption: ParameterOption,
+    ParameterValue: ParameterValue,
+    PopAccount: PopAccount,
+    PortalProfile: PortalProfile,
+    PortalSection: PortalSection,
+    PortalTab: PortalTab,
+    PortalTabSection: PortalTabSection,
+    Portfolio: Portfolio,
+    Predecessor: Predecessor,
+    Preference: Preference,
+    PrivateCustomerPref: PrivateCustomerPref,
+    Program: Program,
+    Project: Project,
+    ProjectUser: ProjectUser,
+    ProjectUserRole: ProjectUserRole,
+    Proof: Proof,
+    QueueDef: QueueDef,
+    QueueTopic: QueueTopic,
+    QueueTopicGroup: QueueTopicGroup,
+    Rate: Rate,
+    Recent: Recent,
+    RecentMenuItem: RecentMenuItem,
+    RecentUpdate: RecentUpdate,
+    RecurrenceRule: RecurrenceRule,
+    RemovedDocumentVersion: RemovedDocumentVersion,
+    ReportFolder: ReportFolder,
+    Reseller: Reseller,
+    ReservedTime: ReservedTime,
+    ResourceAllocation: ResourceAllocation,
+    ResourceContour: ResourceContour,
+    ResourceManager: ResourceManager,
+    ResourcePool: ResourcePool,
+    ResourceSchedulingFilter: ResourceSchedulingFilter,
+    RestorableObject: RestorableObject,
+    Risk: Risk,
+    RiskType: RiskType,
+    Role: Role,
+    RoutingRule: RoutingRule,
+    S3Migration: S3Migration,
+    SSOMapping: SSOMapping,
+    SSOMappingRule: SSOMappingRule,
+    SSOOption: SSOOption,
+    SSOUsername: SSOUsername,
+    SandboxMigration: SandboxMigration,
+    Schedule: Schedule,
+    ScheduledReport: ScheduledReport,
+    ScoreCard: ScoreCard,
+    ScoreCardAnswer: ScoreCardAnswer,
+    ScoreCardOption: ScoreCardOption,
+    ScoreCardQuestion: ScoreCardQuestion,
+    SearchEvent: SearchEvent,
+    SecurityAncestor: SecurityAncestor,
+    Sequence: Sequence,
+    SharingSettings: SharingSettings,
+    StepApprover: StepApprover,
+    Task: Task,
+    Team: Team,
+    TeamMember: TeamMember,
+    TeamMemberRole: TeamMemberRole,
+    Template: Template,
+    TemplateAssignment: TemplateAssignment,
+    TemplatePredecessor: TemplatePredecessor,
+    TemplateTask: TemplateTask,
+    TemplateUser: TemplateUser,
+    TemplateUserRole: TemplateUserRole,
+    TimedNotification: TimedNotification,
+    Timesheet: Timesheet,
+    TimesheetProfile: TimesheetProfile,
+    TimesheetTemplate: TimesheetTemplate,
+    UIFilter: UIFilter,
+    UIGroupBy: UIGroupBy,
+    UIView: UIView,
+    Update: Update,
+    User: User,
+    UserActivity: UserActivity,
+    UserAvailability: UserAvailability,
+    UserDelegation: UserDelegation,
+    UserGroups: UserGroups,
+    UserNote: UserNote,
+    UserObjectPref: UserObjectPref,
+    UserPrefValue: UserPrefValue,
+    UserResource: UserResource,
+    UsersSections: UsersSections,
+    Work: Work,
+    WorkItem: WorkItem
+}
