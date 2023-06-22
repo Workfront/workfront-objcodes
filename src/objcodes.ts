02b148378c3106e854f410633f8b71e8fd3d681d
Artur Vardanyan <avardanyan@adobe.com>
2023-06-22 23:30:43 +0400

    chore: add objCode NLBR

--- src/objcodes.ts
+++ src/objcodes.ts
@@ -136,8 +136,8 @@ export const MetaRecord = 'PRSTOBJ' as const
 export const Milestone = 'MILE' as const
 export const MilestonePath = 'MPATH' as const
 export const MobileDevice = 'MOBILDVC' as const
-export const NonWorkDay = 'NONWKD' as const
 export const NonLaborResource = 'NLBR' as const
+export const NonWorkDay = 'NONWKD' as const
 export const Note = 'NOTE' as const
 export const NoteTag = 'NTAG' as const
 export const NotificationPreference = 'NOTPRF' as const
@@ -363,8 +363,8 @@ export type TObjCode =
     | typeof Milestone
     | typeof MilestonePath
     | typeof MobileDevice
-    | typeof NonWorkDay
     | typeof NonLaborResource
+    | typeof NonWorkDay
     | typeof Note
     | typeof NoteTag
     | typeof NotificationPreference
