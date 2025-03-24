77cac2a31a9d2e64cdcfe50d0ac596ddcfc87726
Hovhannes Babayan <bhovhannes@gmail.com>
2025-03-24 14:09:10 +0400

    chore: remove unnecessary blank line in objcodes.ts

--- src/objcodes.ts
+++ src/objcodes.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 export const AccessLevel = 'ACSLVL' as const
 export const AccessLevelPermissions = 'ALVPER' as const
 export const AccessRequest = 'ACSREQ' as const
